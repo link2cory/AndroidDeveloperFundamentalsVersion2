f98e30941ef34916a8e20bab33198358510a88b4
Cory <link2cory@gmail.com>
2019-01-05 19:24:50 -0800

    Added a SecondActivity which displays a different message based on which of three buttons from MainActivity were clicked
    For now the messages are hard coded but it would be trivial from here to
    add lengthy messages
    
    One key importance is that the id of the button is what is actually sent
    to the SecondActivity, rather than the content of the message

--- app/src/main/java/com/coryperkins/twoactivities/MainActivity.java
+++ app/src/main/java/com/coryperkins/twoactivities/MainActivity.java
@@ -1,20 +1,32 @@
 package com.coryperkins.twoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_BUTTON_ID = "com.coryperkins.twoactivities.extra.button_id";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
+        // create an intent to start the SecondActivity
+        Intent intent = new Intent(this, Main2Activity.class);
+
+        // send the button id to the SecondActivity
+        intent.putExtra(EXTRA_BUTTON_ID, view.getId());
+
+        // start the SecondActivity
+        startActivity(intent);
     }
 }
