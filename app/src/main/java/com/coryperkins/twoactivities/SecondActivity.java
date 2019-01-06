2f13711fbf2db6cd0c0f702101d7427297b82f2a
Cory <link2cory@gmail.com>
2019-01-05 17:59:05 -0800

    Added an EditText view to the MainActivity
    Its content is passed to and displayed in the second activity

--- app/src/main/java/com/coryperkins/twoactivities/SecondActivity.java
+++ app/src/main/java/com/coryperkins/twoactivities/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.coryperkins.twoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // get the message passed from the MainActivity
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // display the message in the text_message view
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
     }
 }
