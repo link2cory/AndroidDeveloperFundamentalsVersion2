7a557b3e6cdbf7fe444874a2b37b8b96168901bf
Cory <link2cory@gmail.com>
2019-01-07 10:34:14 -0800

    Implemented the openWebsite method which now navigates to the website at the url in the website edit text view

--- app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
+++ app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
@@ -1,18 +1,41 @@
 package com.coryperkins.androiddeveloperfundamentalsversion2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // save a reference to the website edit text view
+        mWebsiteEditText = findViewById(R.id.website_edittext);
     }
 
     public void openWebsite(View view) {
+        // setup an intent to view the website in the website edit text
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // verify that there is an activity that can support this action and data combination
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // send the request to view the website
+            startActivity(intent);
+        } else {
+            // print an error message for debugging
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+
     }
 
     public void openLocation(View view) {
