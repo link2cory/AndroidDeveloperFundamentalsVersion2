434bd81818b34a5bb8ee3362d0cdaa6925ec7d1f
Cory <link2cory@gmail.com>
2019-01-13 12:51:38 -0800

    Added a basic layout which includes a text view and a button
    Added some color constants
    
    Added functionality to save and restore the text view's text color
    through activity lifecycle callbacks

---
+++ app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
@@ -0,0 +1,58 @@
+
+package com.coryperkins.androiddeveloperfundamentalsversion2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mHelloTextView;
+    private String[] mColorArray = {
+            "red",
+            "pink",
+            "purple",
+            "deep_purple",
+            "indigo",
+            "blue",
+            "light_blue",
+            "cyan",
+            "teal",
+            "green",
+            "light_green",
+            "lime",
+            "yellow",
+            "amber",
+            "orange",
+            "deep_orange",
+            "brown",
+            "grey",
+            "blue_grey",
+            "black"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // save a reference to the hello text view
+        mHelloTextView = findViewById(R.id.hello_textview);
+
+        // restore saved instance state (the text color)
+        if (savedInstanceState != null) {
+            mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
+    }
+
+    public void changeColor(View view) {
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save the current text color
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
+    }
+}
