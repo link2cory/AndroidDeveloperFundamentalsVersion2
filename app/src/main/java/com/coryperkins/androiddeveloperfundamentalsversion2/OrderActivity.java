fe0badaa73eb74c70d30a0a425a35c2de1600e27
Cory <link2cory@gmail.com>
2019-01-15 14:04:11 -0800

    added radio buttons for delivery method

--- app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/OrderActivity.java
+++ app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/OrderActivity.java
@@ -3,7 +3,10 @@ package com.coryperkins.androiddeveloperfundamentalsversion2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -16,5 +19,32 @@ public class OrderActivity extends AppCompatActivity {
         String message = "Order: " + ((Intent) intent).getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // check if the button is now checked
+        if (((RadioButton) view).isChecked()) {
+            // send the appropriate message based on which button was clicked
+            switch (view.getId()) {
+                case R.id.sameday:
+                    displayToast(getString(R.string.same_day_messenger_service));
+                    break;
+                case R.id.nextday:
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                    break;
+                case R.id.pickup:
+                    displayToast(getString(R.string.pick_up));
+                    break;
+                default:
+                    // should never get here
+                    // do nothing
+                    break;
+            }
+        }
+
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
