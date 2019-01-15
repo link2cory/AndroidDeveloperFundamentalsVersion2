b86dc0251a3fee010b861c880d3a12b9424bf00b
Cory <link2cory@gmail.com>
2019-01-15 14:47:16 -0800

    the phone number edit text now can activate the dialer app
    Obviously this doesn't really make sense, its purely for excercise
    purposes

--- app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/OrderActivity.java
+++ app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/OrderActivity.java
@@ -1,17 +1,23 @@
 package com.coryperkins.androiddeveloperfundamentalsversion2;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    EditText mPhoneText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,23 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
         String message = "Order: " + ((Intent) intent).getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+
+        // setup the ActionListener for the phone dialer
+        mPhoneText = findViewById(R.id.phone_text);
+        if (mPhoneText != null) {
+            mPhoneText.setOnEditorActionListener(
+                new TextView.OnEditorActionListener() {
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        boolean handled = false;
+                        if (actionId == EditorInfo.IME_ACTION_SEND) {
+                            dialNumber();
+                            handled = true;
+                        }
+                        return false;
+                    }
+            });
+        }
 
         // Create the spinner
         Spinner spinner = findViewById(R.id.label_spinner);
@@ -42,6 +65,27 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
         // apply the adapter to the spinner
         if (spinner != null) {
             spinner.setAdapter(adapter);
+        }
+    }
+
+    private void dialNumber() {
+        String phoneNum = null;
+
+        // nothing to do if the phoneText view is null
+        if (mPhoneText != null) {
+            // extract the phone number to dial
+            phoneNum = "tel:" + mPhoneText.getText().toString();
+
+            // create an implicit intent to activate the dialer
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse(phoneNum));
+
+            // attempt to start the activity (only if it resolves)
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            } else {
+                Log.d("OrderActivity", "Implicit intent failed!");
+            }
         }
     }
 
