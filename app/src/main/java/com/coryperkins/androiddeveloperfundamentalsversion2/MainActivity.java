b0e547e00921d56484c7af1476c1eaa4844346a7
Cory <link2cory@gmail.com>
2019-01-14 19:16:22 -0800

    Added event listeners to the dessert items that activate a toast message unique to each item

--- app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
+++ app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +25,34 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    /**
+     * Shows a message that the donut image was clicked
+     */
+    public void showDonutOrder(View view) {
+        displayToast(getString(R.string.donut_order_message));
+    }
+
+    /**
+     * Shows a message that the ice cream image was clicked
+     */
+    public void showIceCreamOrder(View view) {
+        displayToast(getString(R.string.ice_cream_order_message));
+    }
+
+    /**
+     * Shows a message that the froyo image was clicked
+     */
+    public void showFroyoOrder(View view) {
+        displayToast(getString(R.string.froyo_order_message));
+    }
+
+    /**
+     * Display the passed message as a Toast message
+     */
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
 }
