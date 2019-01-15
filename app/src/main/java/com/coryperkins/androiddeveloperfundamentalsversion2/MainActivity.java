76be137d3bfc830fd02996b01b813cd23289529b
Cory <link2cory@gmail.com>
2019-01-15 10:14:15 -0800

    The order activity now displays a message detailing which dessert was most recently ordered

--- app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
+++ app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private String mOrderMessage;
+    public static final String EXTRA_MESSAGE = "com.coryperkins.androidFundamentalsVersion2.extra.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
                 startActivity(intent);
             }
         });
@@ -32,21 +35,24 @@ public class MainActivity extends AppCompatActivity {
      * Shows a message that the donut image was clicked
      */
     public void showDonutOrder(View view) {
-        displayToast(getString(R.string.donut_order_message));
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
     }
 
     /**
      * Shows a message that the ice cream image was clicked
      */
     public void showIceCreamOrder(View view) {
-        displayToast(getString(R.string.ice_cream_order_message));
+        mOrderMessage = getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
     }
 
     /**
      * Shows a message that the froyo image was clicked
      */
     public void showFroyoOrder(View view) {
-        displayToast(getString(R.string.froyo_order_message));
+        mOrderMessage = getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
     }
 
     /**
