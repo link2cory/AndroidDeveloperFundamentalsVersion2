2ad4d41debf1e968a0c1a096dcbcb3e5775bad59
Cory <link2cory@gmail.com>
2019-01-16 12:27:15 -0800

    added a context menu which is activated by longClicking on any of the dessert imageViews

--- app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
+++ app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
@@ -6,22 +6,33 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private String mOrderMessage;
     public static final String EXTRA_MESSAGE = "com.coryperkins.androidFundamentalsVersion2.extra.MESSAGE";
 
+    // ImageViews
+    ImageView donutImageView;
+    ImageView iceCreamImageView;
+    ImageView froyoImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setup the toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // setup the FAB
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +42,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // add context menu to each image
+        donutImageView = findViewById(R.id.donut);
+        iceCreamImageView = findViewById(R.id.ice_cream);
+        froyoImageView = findViewById(R.id.froyo);
+
+        registerForContextMenu(donutImageView);
+        registerForContextMenu(iceCreamImageView);
+        registerForContextMenu(froyoImageView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.context_edit:
+                displayToast(getString(R.string.context_edit_message));
+                return true;
+            case R.id.context_share:
+                displayToast(getString(R.string.context_share_message));
+                return true;
+            case R.id.context_delete:
+                displayToast(getString(R.string.context_delete_message));
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     @Override
