0659ccb2287d5ed4fdc33db047bf1663183f86ba
Cory <link2cory@gmail.com>
2019-01-17 18:14:37 -0800

    added a recyclerView to display all the words

--- app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
+++ app/src/main/java/com/coryperkins/androiddeveloperfundamentalsversion2/MainActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             mWordList.addLast("Word " + i);
         }
-    }
 
+        for (int i = 0; i < 20; i++) {
+            Log.d("MainActivity", mWordList.get(i));
+        }
+
+        // setup the recyclerview
+        // get a handle to the recyclerView
+        mRecyclerView = findViewById(R.id.recyclerview);
+
+        // create an adapter and supply the data to be displayed
+        mAdapter = new WordListAdapter(this, mWordList);
+
+        // connect the adapter with the recyclerView
+        mRecyclerView.setAdapter(mAdapter);
+
+        // give the RecyclerView a default layout manager
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 }
